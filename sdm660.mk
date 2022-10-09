bd2dfe04be6c83e5d8ba7714a969647b593252b3
bilizoekdev <bilizoekdev@gmail.com>
2022-10-09 10:02:09 +0000

    sdm660-common: add overlay-lineage

--- sdm660.mk
+++ sdm660.mk
@@ -7,7 +7,7 @@
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pe \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
